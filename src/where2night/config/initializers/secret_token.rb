71cd9312fc10f4f96a5cd27fd643eaaf65f2aa63
aemonge <andres.monge@gmail.com>
2013-12-04 06:07:18 +0100

    adding the RoR app

---
+++ src/where2night/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Where2night::Application.config.secret_key_base = 'eabb26c2cf2c4c9ab40e4f4145bd378cafd9cbd9732f8b88e755c630fa7a1dc483ba5916c3abd4c8c3e451c872da76ebde053771db14bc2784167091c6beeada'
---
+++ src/where2night/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Where2night::Application.config.session_store :cookie_store, key: '_where2night_session'
